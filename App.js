d4b361f5a85c2495c59cbde86800f1a58afaca48
abbas-77 <abbas.azimii77@gmail.com>
2023-08-15 11:26:16 +0330

    useDeviceOrientation

--- App.js
+++ App.js
@@ -1,79 +1,20 @@
-import react, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  StatusBar,
-  Pressable,
-  Text,
-  Alert,
-  Modal,
-  Image,
-  Button,
-  Dimensions,
-} from "react-native";
+import { StyleSheet, View, StatusBar } from "react-native";
+import { useDeviceOrientation } from "@react-native-community/hooks";
 
 function App() {
-  const [modal, setmodal] = useState(false);
+  const orientation = useDeviceOrientation();
+
   return (
     <View style={styles.container}>
       {/* when you want a hidden or control statusbar use StatusBar */}
       <StatusBar hidden={true} />
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modal}
-        onRequestClose={() => {
-          Alert.alert("modal has been close");
-          setmodal(!modal);
+      <View
+        style={{
+          height: orientation === "portrait" ? "30%" : "100%",
+          backgroundColor: "black",
+          width: "100%",
         }}
-      >
-        <View style={styles.centerView}>
-          <View style={styles.modalView}>
-            <Image
-              source={{ uri: "https://picsum.photos/300/300" }}
-              style={styles.img}
-            />
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => {
-                setmodal(!modal);
-              }}
-            >
-              <Text style={styles.textStyle}>close</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Pressable
-        style={[styles.button, styles.buttonOpen]}
-        onPress={() => {
-          setmodal(!modal);
-          // alert for react-native
-          // Alert.alert("Title", "message for my alert!", [
-          //   {
-          //     text: "YES",
-          //     onPress: () => {
-          //       console.log("press yes");
-          //     },
-          //   },
-          //   {
-          //     text: "NO",
-          //     onPress: () => {
-          //       console.log("press no");
-          //     },
-          //   },
-          // ]);
-        }}
-      >
-        <Text style={styles.textStyle}>Click me!</Text>
-      </Pressable>
-      {/* see the scale and width & height of screen */}
-      <Button
-        title="press"
-        onPress={() => {
-          alert(JSON.stringify(Dimensions.get("screen")));
-        }}
-      />
+      ></View>
     </View>
   );
 }
@@ -82,53 +23,6 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  button: {
-    borderRadius: 25,
-    padding: 15,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: "blue",
-  },
-  buttonClose: {
-    backgroundColor: "red",
-  },
-  textStyle: {
-    fontSize: 20,
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  centerView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 25,
-  },
-  modalView: {
-    backgroundColor: "white",
-    margin: 20,
-    borderRadius: 25,
-    padding: 30,
-    alignItems: "center",
-    //shadow for android
-    elevation: 2,
-    //shadow for ios
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-  },
-  img: {
-    width: 300,
-    height: 300,
-    marginBottom: 10,
   },
 });
 

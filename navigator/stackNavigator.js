3ca1ac3595c7295181c6cd9c8a7f4af5c1d835ad
abbas-77 <abbas.azimii77@gmail.com>
2023-08-15 17:27:46 +0330

    edit header pages

--- navigator/stackNavigator.js
+++ navigator/stackNavigator.js
@@ -6,8 +6,38 @@ import Comments from "../screen/comments";
 function StackNavigator() {
   const Stack = createNativeStackNavigator();
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator
+      //for all page
+      screenOptions={{
+        title: "New Post",
+        headerStyle: { backgroundColor: "green" },
+        headerTintColor: "white",
+        headerTitleStyle: {
+          fontSize: 25,
+          fontWeight: "bold",
+        },
+        headerTitleAlign: "center",
+        //for hide header
+        // headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        //for this page
+        options={{
+          title: "New Post",
+          headerStyle: { backgroundColor: "green" },
+          headerTintColor: "white",
+          headerTitleStyle: {
+            fontSize: 25,
+            fontWeight: "bold",
+          },
+          headerTitleAlign: "center",
+          //for hide header
+          // headerShown: false,
+        }}
+      />
       <Stack.Screen name="Post" component={Post} />
       <Stack.Screen name="Comments" component={Comments} />
     </Stack.Navigator>

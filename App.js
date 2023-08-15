da590495b1c514f43ecc0bd94f43dd02e0fc1abc
abbas-77 <abbas.azimii77@gmail.com>
2023-08-15 15:16:46 +0330

    create first custom hook

--- App.js
+++ App.js
@@ -1,31 +1,8 @@
 import { StyleSheet, View, StatusBar, Text, Button, Image } from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import * as Permission from "expo-permissions";
-import * as Location from "expo-location";
-import { useEffect, useState } from "react";
+import useLocation from "./hooks/useLocation";
 
 function App() {
-  const [location, setLocation] = useState();
-
-  const getLocation = async () => {
-    try {
-      const { granted } = await Location.requestForegroundPermissionsAsync();
-      if (!granted) return;
-      const {
-        coords: { latitude, longitude },
-      } = await Location.getCurrentPositionAsync({
-        accuracy: Location.Accuracy.Highest,
-      });
-      setLocation({ latitude, longitude });
-    } catch (error) {
-      console.log("error:", error);
-    }
-  };
-
-  useEffect(() => {
-    getLocation();
-  }, []);
-
+  const location = useLocation();
   return (
     <View style={styles.container}>
       <Text>{JSON.stringify(location)}</Text>
